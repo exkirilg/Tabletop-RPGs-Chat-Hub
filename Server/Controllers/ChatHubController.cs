ce61f749e49d11818caa039d6b9194b59d305413
kirilg <kirilg.ex@gmail.com>
2022-08-29 15:55:12 +0300

    Implemented basic API chat controller methods

--- Server/Controllers/ChatHubController.cs
+++ Server/Controllers/ChatHubController.cs
@@ -1,37 +1,65 @@
-﻿using Domain.DataAccessInterfaces;
+﻿using Domain.DTO;
+using Domain.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Services.CustomExceptions;
+using Services.Interfaces;
 
 namespace Server.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/chats")]
 [ApiController]
 public class ChatHubController : ControllerBase
 {
-    private const int defNumberOfRooms = 12;
+    private const int defNumberOfChats = 12;
 
-    private IUnitOfWork _unitOfWork;
+    private readonly IChatServices _chatServices;
 
-    public ChatHubController(IUnitOfWork unitOfWork)
+    public ChatHubController(IChatServices chatServices)
     {
-        _unitOfWork = unitOfWork;
-    }
-    
-    [HttpGet("[action]")]
-    public async Task<IActionResult> GetChatRooms(
-        [FromQuery] int numberOfRooms = defNumberOfRooms, [FromQuery] string? search = null)
-    {
-        throw new NotImplementedException();
+        _chatServices = chatServices;
     }
 
+    /// <summary>
+    /// Returns list of active chats
+    /// </summary>
+    /// <param name="numberOfChats"></param>
+    /// <param name="search"></param>
+    /// <returns></returns>
+    /// <response code="200"></response>
+    [HttpGet("ActiveChats")]
+    public async Task<IActionResult> GetActiveChats(
+        [FromQuery] int numberOfChats = defNumberOfChats, [FromQuery] string? search = null)
+    {
+        return Ok((await _chatServices.GetChatsAsync(numberOfChats, search)).Select(chat => chat.ToDTO()));
+    }
+        
+    /// <summary>
+    /// Creates new chat and returns it's value
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    /// <response code="200"></response>
+    /// <response code="400">In case of validation error or if chat with specified name already exists</response>
     [HttpPost("[action]")]
-    public async Task<IActionResult> CreateNewRoom(string roomName)
+    public async Task<IActionResult> CreateNewChat([FromBody(EmptyBodyBehavior = EmptyBodyBehavior.Disallow)] NewChatRequestDTO request)
     {
-        throw new NotImplementedException();
-    }
+        Chat chat;
 
-    [HttpPost("[action]")]
-    public async Task<IActionResult> EnterRoom(string roomName)
-    {
-        throw new NotImplementedException();
+        try
+        {
+            chat = await _chatServices.CreateNewChatAsync(request.Name);
+        }
+        catch (ChatAlreadyExistsException ex)
+        {
+            ModelState.AddModelError(nameof(request.Name), ex.Message);
+            return ValidationProblem();
+        }
+        catch
+        {
+            return BadRequest();
+        }
+
+        return Ok(chat.ToDTO());
     }
 }
