fda4a07b17b27d33c1e143a4cf9f2093971e8397
kirilg <kirilg.ex@gmail.com>
2022-08-30 15:45:22 +0300

    Added send chats info method

--- Server/Hubs/ChatHub.cs
+++ Server/Hubs/ChatHub.cs
@@ -1,9 +1,34 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Services.Interfaces;
 
 namespace Server.Hubs;
 
 public class ChatHub : Hub
 {
+    public const string ReceiveChatsInfoMethod = "ReceiveChatsInfo";
+
+    private readonly IChatServices _chatServices;
+
+    public ChatHub(IChatServices chatServices)
+    {
+        _chatServices = chatServices;
+    }
+
+    public async Task ChatsInfoRequest(int numberOfChats, string? search = default)
+    {
+        await SendChatsInfo(numberOfChats, search);
+    }
+
+    private async Task SendChatsInfo(int numberOfChats, string? search)
+    {
+        await Clients
+            .Client(Context.ConnectionId)
+            .SendAsync(
+                ReceiveChatsInfoMethod,
+                (await _chatServices.GetChatsAsync(numberOfChats, search)).Select(chat => chat.ToDTO())
+            );
+    }
+
     public override async Task OnConnectedAsync()
     {
         await base.OnConnectedAsync();
